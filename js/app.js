83acd8fcc83d637bceca6724a964dde6dd26e93d
salomemateus <salomateus1@gmail.com>
2021-08-05 15:06:54 -0500

    estilos del modal

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function() {
     jQuery('.button__menu').click(function(){
         jQuery(this).toggleClass('menu-open');
         jQuery('header .cont-menu').toggleClass('open');
+        console.log("click");
     });
     //addClass, removeClass, each-->jQuery
 });
